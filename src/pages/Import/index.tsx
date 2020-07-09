bbd70220c55285ca58b9094b010a798dfa3c3fa3
Murilo Henrique <murilohns@gmail.com>
2020-07-09 00:13:40 -0300

    add file upload

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    if (!uploadedFiles) return;
 
     try {
-      // await api.post('/transactions/import', data);
+      uploadedFiles.forEach(async file => {
+        data.append('file', file.file, file.file.name);
+        await api.post('/transactions/import', data);
+
+        history.push('/');
+      });
     } catch (err) {
       // console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const uploadFiles = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(uploadFiles);
   }
 
   return (
